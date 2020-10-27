11b0670ac931e2669bae78f265760545f25a60bd
NikolasKorakidis <korakidisn@gmail.com>
2020-10-27 11:38:21 +0100

    Image Touchables

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, Image, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Image,
+  View,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("hi there");
@@ -8,13 +16,27 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text onPress={handlePress}>Hello!</Text>
-      <Image
-        source={{
-          width: 200,
-          height: 300,
-          uri: "http://picsum.photos/200/300",
-        }}
-      />
+      <TouchableOpacity onPress={() => console.log("hi here")}>
+        <Image
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "http://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity>
+      <TouchableWithoutFeedback onPress={() => console.log("hi here")}>
+        <Image
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "http://picsum.photos/200/300",
+          }}
+        />
+      </TouchableWithoutFeedback>
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
